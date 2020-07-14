105fad4c2baa6b2fa46af0ebb78b25ce826736b9
Sol Ladetzky <solladet@gmail.com>
2020-07-14 15:08:29 -0700

    Initial Commit

---
+++ SpaceMail/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpaceMail
+//
+//  Created by Sol Ladetzky on 7/14/20.
+//  Copyright © 2020 Sol Ladetzky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SpaceMailTests/SpaceMailTests.swift
@@ -0,0 +1,34 @@
+//
+//  SpaceMailTests.swift
+//  SpaceMailTests
+//
+//  Created by Sol Ladetzky on 7/14/20.
+//  Copyright © 2020 Sol Ladetzky. All rights reserved.
+//
+
+import XCTest
+@testable import SpaceMail
+
+class SpaceMailTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
